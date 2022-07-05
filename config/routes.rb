f036f1db9f1c79201173a7d8621c24ef8bb34852
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2022-07-05 14:29:56 +0900

    動作確認用モデルとコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'books/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_052532) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_05_052812) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
